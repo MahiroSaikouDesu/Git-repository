fa814bb54410d53d98fb98687b3a54e043a3108d
Marine_Cat <foxandsgf@foxmail.com>
2024-09-24 16:07:42 +0800

    [Fix]

--- homework/ex1.cpp
+++ homework/ex1.cpp
@@ -106,7 +106,7 @@ ll f(string s)
 
 signed main()
 {
-    // ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
     // Problem 1
     int n, tmp, op = 0; // n 为数组的个数 || op 为排序方式
